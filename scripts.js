9c85fe630833319db378b94e1bd9f5347d714913
antonio <antonyejoao@gmail.com>
2025-03-05 14:59:06 -0300

    criando gitignore e removendo arquivos desnecessários

--- scripts.js
+++ scripts.js
@@ -10,23 +10,3 @@ image_profile.addEventListener("mouseout", event => {
     image_profile.src = "img/minha_imagem.png";
     // console.log("mouse fora da imagem")
 })
-
-// function baixarCurriculo(){
-//     let link = document.createElement("a")
-//     link.href = my_curriculum;
-//     link.download = "Curriculo_Antonio"
-
-//     link.click()
-//     console.log("clicando")
-// }
-// fetch('documents/Curriculo_antonio.pdf')
-//     .then(function(data) {
-//         return data.blob();
-//     }) .then(function(result) {
-//         console.log(result)
-//         imgUrl = URL.createObjectURL(result)
-//         const link = `${imgUrl}`
-//     })
-
-// document.querySelector("#curriculo_para_download").innerHTML = link;
-
