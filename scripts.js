4174170ff97a536807196c6609005595f5bdac46
Antônio de Pádua <Antonyejoao@gmail.com>
2025-03-19 15:59:43 -0300

    ajustando cores do filtro

--- scripts.js
+++ scripts.js
@@ -15,24 +15,37 @@ let imagemFiltro = document.querySelector('.imagem-filtro')
 
 
 let alterarTema = false;
-imagemFiltro.addEventListener('click', function() {
-    if(alterarTema) {
+imagemFiltro.addEventListener('click', function () {
+    if (alterarTema) {
         imagemFiltro.src = "img/filtro-lua.png"
+        document.querySelector('.links').style.color = "#22D4FD";
+        document.querySelector('header').style.backgroundColor = "#272727";
+        document.querySelector('header').style.color = "#F6F6F6";
+        document.querySelector('.btn-linkedin').style.backgroundColor = "transparent";
+        document.querySelector('.btn-linkedin').style.border = "2px solid #22D4FD";
+        document.querySelector('.btn-github').style.backgroundColor = "transparent";
+        document.querySelector('.btn-github').style.border = "2px solid #22D4FD";
+        document.querySelector('body').style.backgroundColor = "#050404";
+        document.querySelector('body').style.color = "#F6F6F6";
+        document.querySelector('footer').style.backgroundColor = "#272727";
     } else {
         imagemFiltro.src = "img/filtro-sol.png"
-        document.querySelector('header').style.backgroundColor = "#3E3F5B"
-        document.querySelector('header').style.color = "#8AB2A6"
-        document.querySelector('.btn-linkedin').style.border = "2px solid #3E3F5B"
-        document.querySelector('.btn-github').style.border = "2px solid #3E3F5B"
-        document.querySelector('body').style.backgroundColor = "#F5ECE0"
+        document.querySelector('.links').style.color = "#8AB2A6";
+        document.querySelector('header').style.backgroundColor = "#3E3F5B";
+        document.querySelector('header').style.color = "#8AB2A6";
+        document.querySelector('.btn-linkedin').style.backgroundColor = "#3E3F5B";
+        document.querySelector('.btn-linkedin').style.border = "2px solid #3E3F5B";
+        document.querySelector('.btn-github').style.backgroundColor = "#3E3F5B";
+        document.querySelector('.btn-github').style.border = "2px solid #3E3F5B";
+        document.querySelector('body').style.backgroundColor = "#F5ECE0";
         document.querySelector('body').style.color = "black";
-        document.querySelector('footer').style.backgroundColor = "#3E3F5B"
+        document.querySelector('footer').style.backgroundColor = "#3E3F5B";
     }
 
     alterarTema = !alterarTema
 })
 
 function alterarFiltro() {
-    
+
     imagemFiltro.src = "img/filtro-sol.png";
 }
